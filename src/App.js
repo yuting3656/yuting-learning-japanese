000a6ad5f629ad870af47135b175738b71d66ae9
yuting <tim23656@gmail.com>
2023-09-21 18:03:09 +0800

    [add] day 07

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 // import Day03 from "./components/day03/Day03";
 // import Day04 from "./components/day04/Day04";
 // import Day05 from "./components/day05/Day05";
-import Day06 from "./components/day06/Day06";
+// import Day06 from "./components/day06/Day06";
+import Day07 from "./components/Day07/Day07";
 
 function App() {
   return (
     <Container maxWidth="xl">
-      <Day06 />
+      <Day07 />
     </Container>
   );
 }
